cfe0f7f821cb79134cc412b9790761aa51e03aa6
Simone Barbieri <odioillatino@me.com>
2012-05-22 18:08:49 +0200

    Revert "Aggiunta degli altri files"
    This reverts commit c036e956da108de5505619e232706cfe1e6f44ef.

--- security/HmacSha256.js
+++
@@ -1,7 +0,0 @@
-function Autenticazione(Message){
-	var pass = "Chiave_fissa_provvisoria";			// In una seconda fase si utilizzerà un protocollo per chiavi.
-	var hash = CryptoJS.HmacSHA256(Message, pass);
-
-	return hash;
-}
-
